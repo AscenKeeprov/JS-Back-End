3126bee702729d821f670ef7acf6d0f8215b2028
Ascen Keeprov <assenkiprov@yahoo.com>
2019-09-21 16:40:56 +0300

    Dropped Express server in favour of Node server

--- CatShelter/app.js
+++ CatShelter/app.js
@@ -1,31 +1,63 @@
-﻿const express = require('express');
-const fileManager = require('./fileManager.js');
+﻿const fileManager = require('./fileManager.js');
+const http = require('http');
 const processManager = require('child_process');
-const app = express();
 
 let config = JSON.parse(fileManager.readFile('./config.json'));
-const server = app.listen(config.port, () => {
-	console.log(`Listening on port ${config.port}`);
+
+const server = http.createServer((request, response) => {
+    request.on('error', (err) => {
+        console.error(err.stack);
+        response.statusCode = 400;
+        response.end();
+    });
+
+    response.on('error', (err) => {
+        console.error(err.stack);
+    });
+
+    if (request.url.includes('/content/')) {
+        fileManager.readFileAsync(`.${request.url}`, (fileData) => {
+            let fileTypeIndex = request.url.lastIndexOf('.');
+            let fileType = request.url.substring(fileTypeIndex + 1);
+            let contentType;
+            switch (fileType.toLowerCase()) {
+                case 'css':
+                    contentType = 'text/css';
+                    break;
+                case 'ico':
+                    contentType = 'image/x-icon';
+                    break;
+                default:
+                    contentType = 'text/plain';
+            }
+            response.setHeader('Content-Type', contentType);
+            response.write(fileData);
+            response.end();
+        });
+    }
+
+    if (request.method === 'GET' && request.url === '/') {
+        fileManager.readFileAsync('./views/home/index.html', (html) => {
+            response.setHeader('Content-Type', 'text/html');
+            response.write(html);
+            response.end();
+        });
+    }
 });
 
-let appURL = `http://localhost:${config.port}`;
+server.listen(config.port, config.host, () => {
+    console.log(`Listening on port ${config.port}`);
+});
+
+let appURL = `http://${config.host}:${config.port}`;
 switch (process.platform) {
-	case 'darwin':
-		processManager.exec(`open ${appURL}`);
-		break;
-	case 'linux':
-		processManager.exec(`xdg-open ${appURL}`);
-		break;
-	case 'win32':
-		processManager.exec(`start ${appURL}`);
-		break;
+    case 'darwin':
+        processManager.exec(`open ${appURL}`);
+        break;
+    case 'linux':
+        processManager.exec(`xdg-open ${appURL}`);
+        break;
+    case 'win32':
+        processManager.exec(`start ${appURL}`);
+        break;
 }
-
-app.get('/', (req, res) => res.sendFile(`${__dirname}/views/home/index.html`));
-app.get('/exit', () => {
-	console.log('Closing application...');
-	server.close();
-	process.exit(0);
-});
-
-app.use(express.static(__dirname));
