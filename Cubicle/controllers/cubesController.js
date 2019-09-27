f3b7527a0e20cb249d1e41f372281ce6f7cf9214
Ascen Keeprov <assenkiprov@yahoo.com>
2019-09-27 17:56:41 +0300

    Implemented local storage for cube entities

--- Cubicle/controllers/cubesController.js
+++ Cubicle/controllers/cubesController.js
@@ -1,4 +1,5 @@
 const Cube = require(`${appRoot}/models/cube.js`);
+const fs = require('fs');
 const ObjectId = require('bson').ObjectId;
 
 function createGet(req, res, next) {
@@ -8,11 +9,33 @@ function createGet(req, res, next) {
 function createPost(req, res, next) {
 	let id = new ObjectId().toString();
 	try {
-		let cube = new Cube(id, req.body.name, req.body.description, req.body.imageURL, req.body.difficulty);
-		console.log(cube);
-		res.redirect('/');
+		let newCube = new Cube(id, req.body.name, req.body.description, req.body.imageURL, req.body.difficulty);
+		const cubesFile = `${appRoot}/data/cubes.json`;
+		fs.readFile(cubesFile, (err, data) => {
+			if (err) throw err;
+			let cubes = JSON.parse(data);
+			let cubeExists = false;
+			for (let cube of cubes) {
+				if (cube._name.toLowerCase().localeCompare(newCube.name.toLowerCase()) === 0) {
+					cubeExists = true;
+					break;
+				}
+			}
+			if (cubeExists) res.render('cubes/create', {
+				error: `A cube named ${newCube.name} already exists!`,
+				title: 'Add a cube',
+			});
+			else {
+				cubes.push(newCube);
+				let json = JSON.stringify(cubes);
+				fs.writeFile(cubesFile, json, 'utf-8', () => {
+					console.log(`Added cube ${newCube.toString()}`);
+				});
+				res.redirect('/');
+			}
+		});
 	} catch (error) {
-		res.render('cubes/create', { title: 'Add a cube', error });
+		res.render('cubes/create', { error, title: 'Add a cube' });
 	}
 }
 
